9807869bd96376716314671b9cb9a768e282d632
diya <diyachandra22@gmail.com>
2024-04-24 01:49:46 +0530

    first

---
+++ frontend/src/components/Thumbnails/Thumbnails.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Price from '../Price/Price';
+import StarRating from '../StarRating/StarRating';
+import classes from './thumbnails.module.css';
+export default function Thumbnails({ foods }) {
+  return (
+    <ul className={classes.list}>
+      {foods.map(food => (
+        <li key={food.id}>
+          <Link to={`/food/${food.id}`}>
+            <img
+              className={classes.image}
+              src={`${food.imageUrl}`}
+              alt={food.name}
+            />
+
+            <div className={classes.content}>
+              <div className={classes.name}>{food.name}</div>
+              <span
+                className={`${classes.favorite} ${
+                  food.favorite ? '' : classes.not
+                }`}
+              >
+                ❤
+              </span>
+              <div className={classes.stars}>
+                <StarRating stars={food.stars} />
+              </div>
+              <div className={classes.fff}>
+                <div className={classes.quantity}>{food.quantity}</div>
+                <div className={classes.product_item_footer}>
+                  <div className={classes.price}>
+                  <Price price={food.price} />
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
---
+++ frontend/src/pages/Checkout/CheckoutPage.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useCart } from '../../hooks/useCart';
+import { useAuth } from '../../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { createOrder } from '../../services/orderService';
+import classes from './checkoutPage.module.css';
+import Title from '../../components/Title/Title';
+import Input from '../../components/Input/Input';
+import Button from '../../components/Button/Button';
+import OrderItemsList from '../../components/OrderItemsList/OrderItemsList';
+export default function CheckoutPage() {
+  const { cart } = useCart();
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const [order, setOrder] = useState({ ...cart });
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const submit = async data => {
+   
+
+    await createOrder({ ...order, name: data.name});
+    navigate('/payment');
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(submit)} className={classes.container}>
+        <div className={classes.content}>
+          <Title title="Order Form" fontSize="1.6rem" />
+          <div className={classes.inputs}>
+            <Input
+              defaultValue={user.name}
+              label="Name"
+              {...register('name')}
+              error={errors.name}
+            />
+            
+          </div>
+          <OrderItemsList order={order} />
+        </div>
+        
+
+        <div className={classes.buttons_container}>
+          <div className={classes.buttons}>
+            <Button
+              type="submit"
+              text="Go To Payment"
+              width="100%"
+              height="3rem"
+              onClick={createOrder}
+            />
+          </div>
+        </div>
+      </form>
+    </>
+  );
+}
